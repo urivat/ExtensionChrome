076920a28723ad000811a35d3caac9adaf6e57cc
urivat <rashad.porter18@gmail.com>
2022-06-10 18:44:49 -0400

    added the ability to go through a array with for loop will display all leads .

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
+const ulEl =  document.getElementById('ul-el')
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
+console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
-
+for ( let i = 0; i < myLeads.length ; i++){
+   ulEl.textContent += myLeads[i]
+}
 
