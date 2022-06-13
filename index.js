af7aa22a3e714a4df65222d7c53971de5924ee8d
urivat <rashad.porter18@gmail.com>
2022-06-13 16:53:45 -0400

    can now add and render new leads to the dom.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl =  document.getElementById('ul-el')
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-console.log(ulEl)
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
-for ( let i = 0; i < myLeads.length ; i++){
-   ulEl.textContent += myLeads[i]
+function renderLeads(){
+    let listItems = ""
+    for ( let i = 0; i < myLeads.length ; i++){
+    
+       listItems +=  "<li>" + myLeads[i]+ "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
 
