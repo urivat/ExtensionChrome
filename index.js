7d2937e79de3413fd81d1ef50b3bfb34d2cdbe4e
urivat <rashad.porter18@gmail.com>
2022-06-14 20:01:51 -0400

    added local storage access to have leads persist over a refresh + add a delete button with functionality and styling

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl =  document.getElementById('ul-el')
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
-function renderLeads(){
-    let listItems = ""
-    for ( let i = 0; i < myLeads.length ; i++){
-    
-       listItems +=  `
-            <li>
-                    <a href=${myLeads[i]}> ${myLeads[i]}</a>
-            </li>
-            `
-    }
-    ulEl.innerHTML = listItems
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  renderLeads()
 }
+deleteBtn.addEventListener('dblclick' , function(){
+  localStorage.clear
+  myLeads = []
+  renderLeads()
+})
 
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+  renderLeads();
+});
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += 
+    `<li>
+        <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]}</a>
+     </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
